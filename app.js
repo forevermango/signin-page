3d363a54b0cd73d681aa54c7989a6322b9657f82
forevermango <raahima95@gmail.com>
2020-11-06 10:00:09 -0600

    adding index.ejs for to-do list

--- app.js
+++ app.js
@@ -39,14 +39,35 @@ app.use(session({
 //placeholders for added task
 const task = ["buy socks", "get money"];
 //placeholders for removed task
-const complete = ["finish jquery"];
+const complete = ["finish homework"];
 
 //post route for adding new task 
 app.post("/addtask", function(req, res) {
-    var newTask = req.body.newtask;
+    let newTask = req.body.newtask;
     //add the new task from the post route
     task.push(newTask);
     res.redirect("/");
+});
+
+app.post("/removetask", function(req, res) {
+    const completeTask = req.body.check;
+    //check for the "typeof" the different completed task, then add into the complete task
+    if (typeof completeTask === "string") {
+        complete.push(completeTask);
+        //check if the completed task already exits in the task when checked, then remove it
+        task.splice(task.indexOf(completeTask), 1);
+    } else if (typeof completeTask === "object") {
+        for (const i = 0; i < completeTask.length; i++) {
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
+        }
+    }
+    res.redirect("/");
+});
+
+//render the ejs and display added task, completed task
+app.get("/", function(req, res) {
+    res.render("index", { task: task, complete: complete });
 });
 
 //Routes
