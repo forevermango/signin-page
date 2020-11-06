78434db33fae0d924881b62dbf977bd05994d4a9
forevermango <raahima95@gmail.com>
2020-11-06 09:12:02 -0600

    added code for port listening

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const router = express.Router();
 const app = express();
 const expressEjsLayout = require('express-ejs-layouts')
-const flash = require('connect-flash');
+//const flash = require('connect-flash');
 const session = require('express-session');
 //mongoose
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ `YOUR DATABASE NAME`;
@@ -25,16 +25,19 @@ app.use(session({
     saveUninitialized : true
    }));
    //use flash
-   app.use(flash());
+/* /*    app.use(flash());
    app.use((req,res,next)=> {
      res.locals.success_msg = req.flash('success_msg');
      res.locals.error_msg = req.flash('error_msg');
      res.locals.error  = req.flash('error');
-   next();
-   })
+   next(); 
+   }) */
    
 //Routes
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
 
 app.listen(3000); 
+
+const PORT = process.env.PORT || 3000 
+app.listen(PORT)
