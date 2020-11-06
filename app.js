b9f0fa8368576a79486971b5bf3e01cb58804e12
forevermango <raahima95@gmail.com>
2020-11-06 07:00:20 -0600

    trying to get login to work

--- app.js
+++ app.js
@@ -16,6 +16,22 @@ app.set('view engine','ejs');
 app.use(expressEjsLayout);
 //BodyParser
 app.use(express.urlencoded({extended : false}));
+
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+   }));
+   //use flash
+   app.use(flash());
+   app.use((req,res,next)=> {
+     res.locals.success_msg = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error  = req.flash('error');
+   next();
+   })
+   
 //Routes
 app.use('/',require('./routes/index'));
 app.use('/users',require('./routes/users'));
